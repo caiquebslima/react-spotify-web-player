4074d907dd6c1b8ce058afa4120b165530868ac4
caiquebslima <caiquebslima@gmail.com>
2021-01-21 07:39:07 -0300

    feat: added icons

---
+++ src/assets/ExpandIcon.jsx
@@ -0,0 +1,13 @@
+export default function ExpandIcon() {
+  return (
+    <svg
+      role='img'
+      height='16'
+      width='16'
+      class='Svg-ulyrgf-0 hJgLcF f6406a56d35aea2a3598f6f270ef156c-scss'
+      viewBox='0 0 16 16'
+    >
+      <path d='M3 6l5 5.794L13 6z'></path>
+    </svg>
+  );
+}
